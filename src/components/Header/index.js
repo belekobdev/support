035bc0dda55c9a79495f11aeb8a59e29d09e9381
unknown <belekobdev>
2023-04-09 16:31:57 +1300

    init

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import img from "../../img/logo.png"
-const Footer = () => {
-    return (
-        <div id="header" className="bg-transparent fixed" >
-            <div className="container w-[90%] mx-auto">
-                <div className="header flex items-center justify-between">
-                    <img src={img} alt=""/>
-                    <div >
-                        <a href="#" className="text-white px-[30px] text-[16px] font-normal" >Главная</a>
-                        <a href="#"  className="text-white text-[16px] font-normal" >Курсы</a>
-                        <a href="#" className="text-white text-[16px] px-[30px] font-normal" >Расписание</a>
-                        <a href="#" className="text-white text-[16px] font-normal" >Преподователь</a>
-                        <a href="#" className="text-white px-[30px] font-normal" >Рассылка</a>
-                        <a href="#" className="text-white text-[16px] font-normal " >Контакты</a>
-                    </div>
+import Hero from "../Hero";
 
+const Footer = () => {
+    const [dark, setDark] = useState(false)
+    return (
+        <>
+            <div id="header" className="py-4" >
+                <div className="container w-[90%] mx-auto">
+                    <div className="header flex items-center justify-between">
+                        <img src={img} alt=""/>
+                        <div >
+                            <a href="#" className="text-white px-[30px] text-[16px] font-normal" style={{
+                                color: dark === true ? "red" : "white"
+                            }} >Главная</a>
+                            <a href="#"  className="text-white text-[16px] font-normal"  style={{
+                                color: dark === true ? "red" : "white"
+                            }}>Курсы</a>
+                            <a href="#" className="text-white text-[16px] px-[30px] font-normal" style={{
+                                color: dark === true ? "red" : "white"
+                            }} >Расписание</a>
+                            <a href="#" className="text-white text-[16px] font-normal" style={{
+                                color: dark === true ? "red" : "white"
+                            }} >Преподователь</a>
+                            <a href="#" className="text-white px-[30px] font-normal" style={{
+                                color: dark === true ? "red" : "white"
+                            }} >Рассылка</a>
+                            <a href="#" className="text-white text-[16px] font-normal " style={{
+                                color: dark === true ? "red" : "white"
+                            }} >Контакты</a>
+                        </div>
+                        <button
+                            className="py-2 px-2 bg-pink-600 text-white rounded-[10px]"  onClick={() => {
+                            setDark(true)
+                        }}>DarkMode</button>
+                    </div>
                 </div>
             </div>
-        </div>
+            <Hero dark={dark}/>
+        </>
     );
 };
 
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Hero = () => {
+const Hero = ({dark}) => {
     return (
         <div id="hero"  >
             <div className="container w-[80%] mx-auto">
@@ -9,22 +9,29 @@ const Hero = () => {
                     по компьютерной сборке</h1>
                     <div className="hero-glav flex mt-14">
                         <div className="hero-card mr-7 w-[100px] h-[102px] bg-neutral-900 flex items-center justify-center flex-col">
-                            <h1 className="text-white text-3xl font-bold">18</h1>
+                            <h1 className="text-white text-3xl font-bold" style={{
+                                color: dark === true ? "red" : "white"
+                            }}>18</h1>
                             <h4 className="text-white opacity-[0.4]">Дней</h4>
                         </div>
                         <div className="hero-card mr-7 w-[102px] h-[102px] bg-neutral-900 flex items-center justify-center flex-col">
-                            <h1 className="text-white text-3xl font-bold">18</h1>
+                            <h1 className="text-white text-3xl font-bold" style={{
+                                color: dark === true ? "red" : "white"
+                            }}>18</h1>
                             <h4 className="text-white opacity-[0.4]">Часов</h4>
                         </div>
                         <div className="hero-card mr-7 w-[102px] h-[102px] bg-neutral-900 flex items-center justify-center flex-col">
-                            <h1 className="text-white text-3xl font-bold">18</h1>
+                            <h1 className="text-white text-3xl font-bold" style={{
+                                color: dark === true ? "red" : "white"
+                            }}>18</h1>
                             <h4 className="text-white opacity-[0.4]">Минут</h4>
                         </div>
                         <div className="hero-card mr-7 w-[102px] h-[102px] bg-neutral-900 flex items-center justify-center flex-col">
-                            <h1 className="text-white text-3xl font-bold">18</h1>
+                            <h1 className="text-white text-3xl font-bold" style={{
+                                color: dark === true ? "red" : "white"
+                            }}>18</h1>
                             <h4 className="text-white opacity-[0.4]">Секунд</h4>
                         </div>
-
                     </div>
                 </div>
             </div>
